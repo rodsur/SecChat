a37b23777ed0c8e682f5534a50428346a0e64c3c
Søren Pedersen <rodsur@rodsur.net>
2013-10-24 20:27:16 +0200

    added threaded send and receive functionality, does however hang on exit due to sockets not being closed

--- client.py
+++ client.py
@@ -1,26 +1,57 @@
 import socket
+import threading
+class ListeningThread(threading.Thread):
+    def __init__(self,client,socket):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.socket = socket
+    def run(self):
+        self.client.Listen(self.socket)
+        pass
+
 class client():
+    active = False
     def __init__(self,PrivateKey,PublicKey):
         self.PrivateKey = PrivateKey
         self.PublicKey = PublicKey
         
     def RunClient(self,IP,Port=5005):
+        self.active = True
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((IP,Port))
-        Message = raw_input("Write message and press enter\n")
-        sock.send(self.PublicKey.public_encrypt(Message,1))
+        ListenThread = ListeningThread(self,sock)
+        ListenThread.start()
+        self.Sender(sock)
+        self.active = False
+#        Message = raw_input("Write message and press enter\n")
+        
 
     def RunServer(self):
-        print "Server not implemented"
+        self.active = True
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(("0.0.0.0",5005))
         sock.listen(1)
         
         conn, addr = sock.accept()
-        print "Connection address:", addr
-        while 1:
-            data = conn.recv(4096)
-            if not data: break
-            print "received data:", self.PrivateKey.private_decrypt(data,1)
-            #conn.send(data)
-        conn.close
+        ListenThread = ListeningThread(self,conn)
+        ListenThread.start()
+        self.Sender(conn)
+        self.active = False
+
+    def Sender(self, socket):
+        while socket:
+            Message = raw_input()
+            if Message == "exit":
+                socket.close
+                break
+            if socket == False:
+                break
+            socket.send(self.PublicKey.public_encrypt(Message,1))
+
+    def Listen(self, socket):
+        if self.active == True:
+            while socket:
+                data = socket.recv(4096)
+                print self.PrivateKey.private_decrypt(data,1)
+        else:
+            socket.close()
